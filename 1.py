c4c6bc8a094ce0f9909c84abb820315ffda47947
Ubuntu <ubuntu@ip-172-31-59-214.ec2.internal>
2021-06-21 05:53:10 +0000

    final project

---
+++ 1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by Lauren Wheatley
+# Created in April 2021
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+def game_scene():
+    # this function is the main game game_scene
+    
+    print("n/n/n") # 3 blank times
+    print("Hello, World!")
+    
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+    
+if __name__ == "__main__":
+    game_scene()
